b6225d231fcfe563eb3a3201622f687a2f3b3b69
agneev <a4ghosh@eng.ucsd.edu>
2016-04-25 17:56:01 -0700

    fin

--- lib/src/main/java/com/example/serv/App.java
+++ lib/src/main/java/com/example/serv/App.java
@@ -19,9 +19,9 @@ public class App {
     public static Content createContent() {
         Content c = new Content();
 
-        c.addRegId("APA91bGNKqkUQsqv3-Yjh2f84U8Z9v4z6hQ1XUy2g9pDztwGcB28SITCZFBPKgUGVJOvu-oYUyt--fdKiEuxq45vJRZjPYrvYUXQOe9Q0xmBpwofNUTjVNitQW3A1D16rlN0xeoszsZa");
+        c.addRegId("APA91bGB-ZbeyCtLP_QRNQE5zu4Y-1LgRlTFiIMh2vDrhtgVwL5vTo5DYiE_YFo4ftOjxk4Nihykt_LSTPL9T_7FX64I7EQoe-l_eQ_4-T36BwAdOgAioktNUE1fwFoaPe6fdzQ1Aayx");
 
-        c.createData("Working!! BALLE BALLE", "Test message");
+        c.createData("Working!!", "Test message");
 
         return c;
     }
